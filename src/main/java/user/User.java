230027807943dddb0f747d15739b0ffba086dabc
Andrew Omondi <andrew.omondi@m-kopa.com>
2018-03-26 21:50:33 +0300

    Initial commit with working post method for users saving into DB

---
+++ src/main/java/user/User.java
@@ -0,0 +1,54 @@
+package user;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+    @Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+
+	@NotNull
+	private String phone;
+	@NotNull
+    private String name;
+	@NotNull
+	private String password;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
+
